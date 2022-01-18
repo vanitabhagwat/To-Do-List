5f5d231466daf7e78d26c3336f437ede6efebe95
chandan sharma <vanitabhagwat@gmail.com>
2022-01-18 00:10:02 -0600

    delete bug fix

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -14,5 +14,6 @@ export function updateTask (id, task) {
 }
 
 export function deleteTask (id){
-    return axios.delete(apiUrl + "/", + id)
+    console.log(id)
+    return axios.delete(apiUrl + "/" + id)
 }
